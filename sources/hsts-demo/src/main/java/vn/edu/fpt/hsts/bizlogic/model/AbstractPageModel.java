688a60439654c5dd232c3b493a42f8850d7c1c98
dangquantran93 <dangquantran93@gmail.com>
2015-09-25 13:51:43 +0700

    finish sample code for server: JpaRepository, hql, native query, web service request mapping

---
+++ sources/hsts-demo/src/main/java/vn/edu/fpt/hsts/bizlogic/model/AbstractPageModel.java
@@ -0,0 +1,11 @@
+/**
+ * FPT University Capstone Project 2015.
+ * Program: HSTS.
+ * Program manager: Kieu Trong Khanh.
+ * Author: dangquantran.
+ * Date: 9/25/2015.
+ */
+package vn.edu.fpt.hsts.bizlogic.model;
+
+public class AbstractPageModel {
+}
