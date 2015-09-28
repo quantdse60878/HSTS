a632aae15dc2d4ae4e4bc0178be81c9eec4b1fd6
Phan Nhat Anh <anhpnse90158@fpt.edu.vn>
2015-09-28 15:53:36 +0700

    add historyTreatment

--- sources/hsts-demo/src/main/java/vn/edu/fpt/hsts/web/DoctorController.java
+++ sources/hsts-demo/src/main/java/vn/edu/fpt/hsts/web/DoctorController.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import vn.edu.fpt.hsts.common.IConsts;
 
@@ -21,12 +22,33 @@ import vn.edu.fpt.hsts.common.IConsts;
 public class DoctorController {
     private static final Logger LOGGER = LoggerFactory.getLogger(DoctorController.class);
 
+    /**
+     * The doctor patients page mapping
+     * @return
+     */
     @RequestMapping(value = "doctorPatients", method = RequestMethod.GET)
     public ModelAndView doctorPatientsPage() {
         LOGGER.info(IConsts.BEGIN_METHOD);
         try {
             ModelAndView modelAndView = new ModelAndView();
             modelAndView.setViewName("doctorPatients");
+            return modelAndView;
+        } finally {
+            LOGGER.info(IConsts.END_METHOD);
+        }
+    }
+
+    /**
+     * The history treatment page mapping
+     * @param patientID
+     * @return
+     */
+    @RequestMapping(value = "historyTreatment", method = RequestMethod.GET)
+    public ModelAndView historyTreatmentPage(@RequestParam("patientID") final String patientID) {
+        LOGGER.info(IConsts.BEGIN_METHOD);
+        try {
+            ModelAndView modelAndView = new ModelAndView();
+            modelAndView.setViewName("historyTreatment");
             return modelAndView;
         } finally {
             LOGGER.info(IConsts.END_METHOD);
