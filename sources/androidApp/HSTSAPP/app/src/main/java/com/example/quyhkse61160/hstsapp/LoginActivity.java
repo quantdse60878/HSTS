bb80684d9c4a819f4192098e92078d65a15129e6
Ha Kim Quy <ruybiking@gmail.com>
2015-09-29 15:40:55 +0700

    [Source] fix some conflict

--- sources/androidApp/HSTSAPP/app/src/main/java/com/example/quyhkse61160/hstsapp/LoginActivity.java
+++ sources/androidApp/HSTSAPP/app/src/main/java/com/example/quyhkse61160/hstsapp/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.quyhkse61160.hstsapp;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.CharacterPickerDialog;
@@ -11,8 +12,24 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.quyhkse61160.hstsapp.Common.Constant;
+import com.example.quyhkse61160.hstsapp.Common.HSTSUtils;
+import com.loopj.android.http.RequestParams;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import cz.msebera.android.httpclient.NameValuePair;
+import cz.msebera.android.httpclient.message.BasicNameValuePair;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -27,26 +44,29 @@ public class LoginActivity extends AppCompatActivity {
         }
 
         setContentView(R.layout.activity_main);
-        EditText txtUsername = (EditText) findViewById(R.id.txt_login_username);
-        EditText txtPassword = (EditText) findViewById(R.id.txt_login_password);
+        final EditText txtUsername = (EditText) findViewById(R.id.txt_login_username);
+        final EditText txtPassword = (EditText) findViewById(R.id.txt_login_password);
         Button btnLogin = (Button) findViewById(R.id.btn_login_login);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                if(txtUsername.getText().length() > 0 && txtPassword.getText().length() > 0) {
+
+                    LoginAsyncTask login = new LoginAsyncTask();
+                    login.execute(txtUsername.getText().toString(), txtPassword.getText().toString());
+
+
+                } else {
+                    Toast.makeText(LoginActivity.this, "Vui lòng nhập đầy đủ thông tin.", Toast.LENGTH_SHORT).show();
+                }
+
+
+                Intent intent = new Intent(LoginActivity.this, DeviceScanActivity.class);
+                //Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
                 startActivity(intent);
             }
         });
 
-        //KhuongMH
-        Button btnRegister = (Button) findViewById(R.id.btn_login_register);
-        btnRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // ?
-            }
-        });
-        //KhuongMH
     }
 
     @Override
@@ -70,6 +90,57 @@ public class LoginActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private class LoginAsyncTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            String stringURL = Constant.hostURL + Constant.loginMethod;
+
+            try {
+                URL url = new URL(stringURL);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setReadTimeout(100000);
+                urlConnection.setConnectTimeout(30000);
+                urlConnection.setRequestMethod("POST");
+                urlConnection.setDoInput(true);
+                urlConnection.setDoOutput(true);
+
+                List<NameValuePair> params = new ArrayList<NameValuePair>();
+                params.add(new BasicNameValuePair("username", strings[0]));
+                params.add(new BasicNameValuePair("password", strings[1]));
+
+                OutputStream os = urlConnection.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+                writer.write(HSTSUtils.getQuery(params));
+                writer.flush();
+                writer.close();
+                os.close();
+
+                urlConnection.connect();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+            return "";
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+
+
+        }
+    }
+
+
+
+
+
 
 
 }
