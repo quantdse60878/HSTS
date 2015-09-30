c419d27e963d2e93a90e0c9f4212f8d8375d458d
Ha Kim Quy <ruybiking@gmail.com>
2015-09-30 13:46:19 +0700

    [Source] Update android application

---
+++ sources/androidApp/HSTSAPP/app/src/main/java/com/example/quyhkse61160/hstsapp/Adapter/PracticeAdapter.java
@@ -0,0 +1,65 @@
+package com.example.quyhkse61160.hstsapp.Adapter;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.quyhkse61160.hstsapp.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by QUYHKSE61160 on 9/30/2015.
+ */
+public class PracticeAdapter extends BaseAdapter {
+
+    private Activity activity;
+    private ArrayList<HashMap<String, String>> data;
+    private static LayoutInflater inflater = null;
+
+    public PracticeAdapter(Activity activity, ArrayList<HashMap<String, String>> data) {
+        this.activity = activity;
+        this.data = data;
+        inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return data.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return data.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View vi = convertView;
+        if(vi == null) {
+            vi = inflater.inflate(R.layout.home_fragment_tab_3_item, null);
+        }
+        TextView txtFoodName = (TextView) vi.findViewById(R.id.item_practice_name);
+        TextView txtQuantitativeOfMedicine = (TextView) vi.findViewById(R.id.item_time_practice);
+
+
+        HashMap<String, String> item = new HashMap<>();
+        item = data.get(position);
+
+        txtFoodName.setText(item.get("PracticeName"));
+        txtQuantitativeOfMedicine.setText(item.get("PracticeTime"));
+
+
+        return vi;
+    }
+}
--- sources/androidApp/HSTSAPP/app/src/main/java/com/example/quyhkse61160/hstsapp/Fragment/Tab3.java
+++ sources/androidApp/HSTSAPP/app/src/main/java/com/example/quyhkse61160/hstsapp/Fragment/Tab3.java
@@ -6,14 +6,22 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
+import com.example.quyhkse61160.hstsapp.Adapter.MedicineAdapter;
+import com.example.quyhkse61160.hstsapp.Adapter.PracticeAdapter;
 import com.example.quyhkse61160.hstsapp.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class Tab3 extends Fragment {
 
+    PracticeAdapter adapter;
+    ListView listView;
 
     public Tab3() {
         // Required empty public constructor
@@ -23,7 +31,28 @@ public class Tab3 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.home_fragment_tab_3, container, false);
+        View v = inflater.inflate(R.layout.home_fragment_tab_3, container, false);
+        ArrayList<HashMap<String,String>> sections = new ArrayList<HashMap<String,String>>();
+        HashMap<String,String> d = new HashMap<>();
+        d.put("PracticeName", "Đi bộ");
+        d.put("PracticeTime", "15");
+        sections.add(d);
+        d = new HashMap<>();
+        d.put("PracticeName", "Chạy bộ");
+        d.put("PracticeTime", "20");
+        sections.add(d);
+        d = new HashMap<>();
+        d.put("PracticeName", "Đạp xe đạp");
+        d.put("PracticeTime", "10");
+        sections.add(d);
+        d = new HashMap<>();
+        d.put("PracticeName", "Chơi đá bóng");
+        d.put("PracticeTime", "15");
+        sections.add(d);
+        listView = (ListView) v.findViewById(R.id.list_practice_treatment);
+        adapter = new PracticeAdapter(getActivity(), sections);
+        listView.setAdapter(adapter);
+        return v;
     }
 
 
