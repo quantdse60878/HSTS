f7af280399bf691430b2f8d89da26c3cefcd454e
Ha Kim Quy <ruybiking@gmail.com>
2015-10-07 07:24:51 +0700

    [Source] Server treatment service

--- sources/hsts-demo/src/main/java/vn/edu/fpt/hsts/bizlogic/model/TreatmentModel.java
+++ sources/hsts-demo/src/main/java/vn/edu/fpt/hsts/bizlogic/model/TreatmentModel.java
@@ -1,6 +1,7 @@
 package vn.edu.fpt.hsts.bizlogic.model;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -8,6 +9,11 @@ import java.util.List;
  */
 public class TreatmentModel {
 
+    private String illnessName;
+    private Date nextAppointment = new Date();
+    private Date fromDate = new Date();
+    private Date toDate = new Date();
+    private String advice;
     private List<FoodTreatmentModel> listFoodTreatment = new ArrayList<FoodTreatmentModel>();
     private List<MedicineTreatmentModel> listMedicineTreatment = new ArrayList<MedicineTreatmentModel>();
     private List<PracticeTreatmentModel> listPracticeTreatment = new ArrayList<PracticeTreatmentModel>();
@@ -15,10 +21,55 @@ public class TreatmentModel {
     public TreatmentModel() {
     }
 
-    public TreatmentModel(List<FoodTreatmentModel> listFoodTreatment, List<MedicineTreatmentModel> listMedicineTreatment, List<PracticeTreatmentModel> listPracticeTreatment) {
+    public TreatmentModel(String illnessName, Date nextAppointment, Date fromDate, Date toDate, String advice, List<FoodTreatmentModel> listFoodTreatment, List<MedicineTreatmentModel> listMedicineTreatment, List<PracticeTreatmentModel> listPracticeTreatment) {
+        this.illnessName = illnessName;
+        this.nextAppointment = nextAppointment;
+        this.fromDate = fromDate;
+        this.toDate = toDate;
+        this.advice = advice;
         this.listFoodTreatment = listFoodTreatment;
         this.listMedicineTreatment = listMedicineTreatment;
         this.listPracticeTreatment = listPracticeTreatment;
+    }
+
+    public String getIllnessName() {
+        return illnessName;
+    }
+
+    public void setIllnessName(String illnessName) {
+        this.illnessName = illnessName;
+    }
+
+    public Date getNextAppointment() {
+        return nextAppointment;
+    }
+
+    public void setNextAppointment(Date nextAppointment) {
+        this.nextAppointment = nextAppointment;
+    }
+
+    public Date getFromDate() {
+        return fromDate;
+    }
+
+    public void setFromDate(Date fromDate) {
+        this.fromDate = fromDate;
+    }
+
+    public Date getToDate() {
+        return toDate;
+    }
+
+    public void setToDate(Date toDate) {
+        this.toDate = toDate;
+    }
+
+    public String getAdvice() {
+        return advice;
+    }
+
+    public void setAdvice(String advice) {
+        this.advice = advice;
     }
 
     public List<FoodTreatmentModel> getListFoodTreatment() {
---
+++ sources/hsts-demo/src/main/java/vn/edu/fpt/hsts/bizlogic/service/TreatmentService.java
@@ -0,0 +1,7 @@
+package vn.edu.fpt.hsts.bizlogic.service;
+
+/**
+ * Created by QUYHKSE61160 on 10/6/15.
+ */
+public class TreatmentService {
+}
