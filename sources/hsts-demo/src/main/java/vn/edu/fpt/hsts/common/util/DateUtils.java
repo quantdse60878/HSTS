4dcd7146a7ed24ea990329b567b9bec33de2004b
dangquantran93 <dangquantran93@gmail.com>
2015-10-08 07:45:42 +0700

    update

--- sources/hsts-demo/src/main/java/vn/edu/fpt/hsts/common/util/DateUtils.java
+++ sources/hsts-demo/src/main/java/vn/edu/fpt/hsts/common/util/DateUtils.java
@@ -7,5 +7,21 @@
  */
 package vn.edu.fpt.hsts.common.util;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Util class for working with date, timestamp type
+ */
 public class DateUtils {
+
+    public static final String DATE_PATTERN_1 = "dd/MM/yyyy";
+
+    public static final String DATE_PATTERN_2 = "dd-MM-yyyy";
+
+    public static String formatDate(final Date date, final String datePattern) {
+        final DateFormat dateFormat = new SimpleDateFormat(datePattern);
+        return dateFormat.format(date);
+    }
 }
