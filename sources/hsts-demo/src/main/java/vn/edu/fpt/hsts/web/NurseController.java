33e7d45cf88dc4656ff238c3c1c9dcbd8a1581c5
dangquantran93 <dangquantran93@gmail.com>
2015-10-09 12:39:04 +0700

    update

---
+++ sources/hsts-demo/src/main/java/vn/edu/fpt/hsts/web/NurseController.java
@@ -0,0 +1,11 @@
+/**
+ * FPT University Capstone Project 2015.
+ * Program: HSTS.
+ * Program manager: Kieu Trong Khanh.
+ * Author: dangquantran.
+ * Date: 10/9/2015.
+ */
+package vn.edu.fpt.hsts.web;
+
+public class NurseController {
+}
