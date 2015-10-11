247907809711ef58ad2e68649057b0e751816662
Phan Nhat Anh <anhpnse90158@fpt.edu.vn>
2015-10-11 15:39:37 +0700

    update patient of nurse

--- sources/hsts-demo/src/main/java/vn/edu/fpt/hsts/web/NurseController.java
+++ sources/hsts-demo/src/main/java/vn/edu/fpt/hsts/web/NurseController.java
@@ -13,9 +13,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import vn.edu.fpt.hsts.bizlogic.model.DoctorModel;
+import vn.edu.fpt.hsts.bizlogic.service.DoctorService;
 import vn.edu.fpt.hsts.bizlogic.service.PatientService;
 import vn.edu.fpt.hsts.common.IConsts;
+import vn.edu.fpt.hsts.common.expception.BizlogicException;
+import vn.edu.fpt.hsts.criteria.PatientCriteria;
 import vn.edu.fpt.hsts.persistence.entity.Patient;
 
 import java.util.List;
@@ -27,15 +32,78 @@ public class NurseController extends AbstractController {
     @Autowired
     PatientService patientService;
 
+    @Autowired
+    DoctorService doctorService;
+
     @RequestMapping(value = "updatePatient", method = RequestMethod.GET)
-    public ModelAndView updatePatientsPage() {
+    public ModelAndView nursePatientsPage() {
+        LOGGER.info(IConsts.BEGIN_METHOD);
+        try {
+            ModelAndView mav = new ModelAndView();
+            mav.setViewName("nursePatients");
+            List<Patient> patientList = patientService.getAllPatients();
+            LOGGER.info("listpatiens: " + patientList.size());
+            mav.addObject("LISTPATIENTS", patientList);
+            return mav;
+        } finally {
+            LOGGER.info(IConsts.END_METHOD);
+        }
+    }
+
+    @RequestMapping(value = "patient", method = RequestMethod.GET)
+    public ModelAndView updatePatientPage(@RequestParam("patientID") final int patientID) {
         LOGGER.info(IConsts.BEGIN_METHOD);
         try {
             ModelAndView mav = new ModelAndView();
             mav.setViewName("updatePatient");
-            List<Patient> patientList = patientService.getAllPatients();
-            LOGGER.info("listpatiens: " + patientList.size());
-            mav.addObject("LISTPATIENTS", patientList);
+            Patient patient = patientService.getPatientByID(patientID);
+            mav.addObject("PATIENT", patient);
+            return mav;
+        } finally {
+            LOGGER.info(IConsts.END_METHOD);
+        }
+    }
+
+    @RequestMapping(value = "updatePatient", method = RequestMethod.POST)
+    public ModelAndView registerPatient(@RequestParam("patientID") final int patientID,
+                                        @RequestParam("weight") final int weight,
+                                        @RequestParam("height") final int height,
+                                        @RequestParam("doctorId") final int doctorId,
+                                        @RequestParam(value = "medicalHistory") final String medicalHistory,
+                                        @RequestParam(value = "symptoms") final String symptoms) throws BizlogicException, BizlogicException {
+        LOGGER.info(IConsts.BEGIN_METHOD);
+        try {
+//            LOGGER.info("patientName[{}], email[{}], birthday[{}], gender[{}], weight[{}], height[{}], doctorId[{}], medicalHistory[{}], symptoms[{}]",
+//                    patientName, email, birthday, gender, weight, height, doctorId, medicalHistory, symptoms);
+
+            ModelAndView mav = new ModelAndView();
+            mav.setViewName("updatePatient");
+            Patient patient = patientService.getPatientByID(patientID);
+            mav.addObject("PATIENT", patient);
+            /**
+             * Update patient
+             */
+            PatientCriteria criteria = new PatientCriteria();
+            criteria.setWeight(weight);
+            criteria.setHeight(height);
+            criteria.setDoctorId(doctorId);
+            criteria.setMedicalHistory(medicalHistory);
+            criteria.setSymptom(symptoms);
+
+//            patientService.createPatient(criteria);
+
+            // Add doctors to request
+            final List<DoctorModel> doctors = doctorService.findAll();
+            mav.addObject("DOCTORS", doctors);
+
+            //create notify
+            //set name of action
+            mav.addObject("METHOD", "Update Patient's Profile");
+            //set type. sussces TYPE = info, fail TYPE = danger
+            mav.addObject("TYPE", "info");
+            //set message notify
+            mav.addObject("MESSAGE","Success");
+
             return mav;
         } finally {
             LOGGER.info(IConsts.END_METHOD);
