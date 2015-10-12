774a471afc61c24e45318088a4b91716f986bc5f
Phan Nhat Anh <anhpnse90158@fpt.edu.vn>
2015-10-12 20:36:18 +0700

    ajax notify

---
+++ sources/hsts-demo/src/main/java/vn/edu/fpt/hsts/web/AjaxController.java
@@ -0,0 +1,39 @@
+package vn.edu.fpt.hsts.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+import vn.edu.fpt.hsts.bizlogic.service.NotifyService;
+import vn.edu.fpt.hsts.common.IConsts;
+import vn.edu.fpt.hsts.persistence.entity.Patient;
+
+import java.util.List;
+
+/**
+ * Created by Aking on 10/12/2015.
+ */
+@Controller
+public class AjaxController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AjaxController.class);
+
+    @Autowired
+    NotifyService notifyService;
+
+    @RequestMapping(value = "notifyWeb", method = RequestMethod.GET)
+    @ResponseBody
+    public String notifyAjax() {
+        LOGGER.info(IConsts.BEGIN_METHOD);
+        try {
+            String json = "test";
+
+            return json;
+        } finally {
+            LOGGER.info(IConsts.END_METHOD);
+        }
+    }
+}
