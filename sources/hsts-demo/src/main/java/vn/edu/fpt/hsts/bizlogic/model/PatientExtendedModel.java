7125f39cb7d7033109aabba1cea22368b947ce2e
dangquantran93 <dangquantran93@gmail.com>
2015-10-12 14:27:21 +0700

    update

---
+++ sources/hsts-demo/src/main/java/vn/edu/fpt/hsts/bizlogic/model/PatientExtendedModel.java
@@ -0,0 +1,11 @@
+/**
+ * FPT University Capstone Project 2015.
+ * Program: HSTS.
+ * Program manager: Kieu Trong Khanh.
+ * Author: dangquantran.
+ * Date: 10/12/2015.
+ */
+package vn.edu.fpt.hsts.bizlogic.model;
+
+public class PatientExtendedModel {
+}
---
+++ sources/hsts-demo/src/main/resources/static/js/nursePatients.js
@@ -0,0 +1,7 @@
+/**
+ * FPT University Capstone Project 2015.
+ * Program: HSTS.
+ * Program manager: Kieu Trong Khanh.
+ * Author: dangquantran.
+ * Date: 10/12/2015.
+ */
