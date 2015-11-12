18172ba7c23825f656949e34f7aa7b299258479d
dangquantran93 <dangquantran93@gmail.com>
2015-11-12 18:48:45 +0700

    update

---
+++ sources/hsts-demo/src/main/resources/static/js/detailPhase.js
@@ -0,0 +1,15 @@
+/**
+ * FPT University Capstone Project 2015.
+ * Program: HSTS.
+ * Program manager: Kieu Trong Khanh.
+ * Author: dangquantran.
+ * Date: 11/12/2015.
+ */
+function changeTab (a, li) {
+    console.log("a: " + a);
+    console.log("li: " + li);
+    $('.nav-tabs > li.active').removeClass('active');
+    $(li).addClass('active');
+    $('.tab-content > .tab-pane.active').removeClass('active');
+    $(a).addClass('active');
+};
