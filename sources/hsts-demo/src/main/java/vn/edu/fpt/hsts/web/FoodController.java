ffbeff75ebfa31f1aaeb980f5aa8f8cb6fbca9b6
dangquantran93 <dangquantran93@gmail.com>
2015-11-14 14:48:22 +0700

    update

---
+++ sources/hsts-demo/src/main/java/vn/edu/fpt/hsts/web/FoodController.java
@@ -0,0 +1,23 @@
+/**
+ * FPT University Capstone Project 2015.
+ * Program: HSTS.
+ * Program manager: Kieu Trong Khanh.
+ * Author: dangquantran.
+ * Date: 11/14/2015.
+ */
+package vn.edu.fpt.hsts.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class FoodController extends AbstractController {
+
+    /**
+     * The logger.
+     */
+    private static final Logger LOGGER = LoggerFactory.getLogger(FoodController.class);
+
+
+}
