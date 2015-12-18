13cb3721f4e4d90b0984d0a55f2398b08a91b11e
dangquantran93 <dangquantran93@gmail.com>
2015-12-18 15:14:50 +0700

    update

--- sources/hsts-demo/src/main/resources/static/js/patientProfile.js
+++ sources/hsts-demo/src/main/resources/static/js/patientProfile.js
@@ -43,7 +43,60 @@ $(document).ready(function(){
                 // Body
                 bodyContent += '<div class="panel-collapse collapse" id="collapse' + count + '">';
                 bodyContent += '<div class="panel-body">';
-                bodyContent += 'Full content here';
+
+                bodyContent += '<div class="col-md-12">';
+
+
+                bodyContent += '<div class="form-group">';
+                bodyContent += '<label class="col-sm-3 control-label">Doctor:</label>';
+                bodyContent += '<div class="col-sm-3">';
+                bodyContent += '<input class = "hsts" type="text" readonly="readonly" value="' + element.doctor.account.fullName + '" />';
+                bodyContent += '</div>';
+                bodyContent += '</div>';
+
+                bodyContent += '<div class="form-group">';
+                bodyContent += '<label class="col-sm-3 control-label">Illness:</label>';
+                bodyContent += '<div class="col-sm-3">';
+                bodyContent += '<input class = "hsts" type="text" readonly="readonly" value="' + element.illness.name + '" />';
+                bodyContent += '</div>';
+                bodyContent += '</div>';
+
+                bodyContent += '<div class="form-group">';
+                bodyContent += '<label class="col-sm-3 control-label">Symptoms:</label>';
+                bodyContent += '<div class="col-sm-5">';
+                bodyContent += '<input class = "hsts" type="text" readonly="readonly" value="' + element.symptoms + '" />';
+                bodyContent += '</div>';
+                bodyContent += '</div>';
+
+                //status
+                bodyContent += '<div class="form-group">';
+                bodyContent += '<label class="col-sm-3 control-label">Symptoms:</label>';
+                bodyContent += '<div class="col-sm-5">';
+                bodyContent += '<input class = "hsts" type="text" readonly="readonly" value="' + element.symptoms + '" />';
+                bodyContent += '</div>';
+                bodyContent += '</div>';
+
+                // view button
+                var btnClass = 'btn btn-warning';
+                var btnInfo = 'AWAITING TO EXAM';
+                if (element.status == 2) {
+                    btnClass = 'btn btn-danger';
+                    btnInfo = 'ON TREATING';
+                } else if (element.status == 3) {
+                    btnClass = 'btn btn-success';
+                    btnInfo = 'NO ILLNESS';
+                } else if (element.status == 4) {
+                    btnClass = 'btn btn-success';
+                    btnInfo = 'FINISHED';
+                }
+                bodyContent += '<div class="form-group">';
+                bodyContent += '<label class="col-sm-3 control-label">Symptoms:</label>';
+                bodyContent += '<div class="col-sm-5">';
+                bodyContent += '<a href = "/appointmentsByMR?id='+ element.id + '" class="'+ btnClass + '">'+ btnInfo + '</a>';
+                bodyContent += '</div>';
+                bodyContent += '</div>';
+
+                bodyContent += '</div>';
                 bodyContent += '</div>';
                 bodyContent += '</div>';
                 // .Body
